359aa28f8ba2b18ed136c8aab6c1a1be57a1f7fc
OfficialURL <eric.ivan.hdz@gmail.com>
2020-05-26 23:24:41 -0500

    Fied Point.intersect functionality

--- miratope.js
+++ miratope.js
@@ -602,7 +602,7 @@ class Point {
 	}
 	
 	//Scales up the point x by a factor of t.
-	static multiply(x, t) {
+	static multiplyBy(x, t) {
 		var coordinates = [];
 		for(var i = 0; i < x.dimensions(); i++)
 			coordinates[i] = x.coordinates[i] * t;
@@ -610,7 +610,7 @@ class Point {
 	}
 	
 	//Scales up the point x by a factor of 1/t.
-	static multiply(x, t) {
+	static divideBy(x, t) {
 		var coordinates = [];
 		for(var i = 0; i < x.dimensions(); i++)
 			coordinates[i] = x.coordinates[i] / t;
@@ -630,39 +630,46 @@ class Point {
 	static intersect(a, b, c, d) {
 		if(a.dimensions() !== b.dimensions() || a.dimensions() !== c.dimensions() || a.dimensions() !== d.dimensions())
 			throw new Error("You can't intersect edges with different amounts of dimensions!");
+		
 		//First, we calculate the coordinates in which each line segment coordinates change the most.
 		//That way, we avoid projecting lines into points.
-		var ab_MAX = 0, ab_MAX_indx = 0, cd_MAX = 0, cd_MAX_indx = 0;
-		for(var i = 0; i < a.dimensions(); i++) {
-			var ab = Math.abs(a.coordinates[i] - b.coordinates[i]);
-			var cd = Math.abs(c.coordinates[i] - d.coordinates[i]);
-			if(ab > ab_MAX){
-				ab_MAX = ab;
-				ab_MAX_indx = i;
+		//No need to do it in 2D, though.
+		var ab_MAX = 0, ab_MAX_indx = 0, cd_MAX = 0, cd_MAX_indx = 1;
+		if(a.dimensions() !== 2) {
+			
+			for(var i = 0; i < a.dimensions(); i++) {
+				var ab = Math.abs(a.coordinates[i] - b.coordinates[i]);
+				var cd = Math.abs(c.coordinates[i] - d.coordinates[i]);
+				if(ab > ab_MAX){
+					ab_MAX = ab;
+					ab_MAX_indx = i;
+				}
+				if(cd > cd_MAX){
+					cd_MAX = cd;
+					cd_MAX_indx = i;
+				}
 			}
-			if(cd > cd_MAX){
-				cd_MAX = cd;
-				cd_MAX_indx = i;
+			
+			//If both indices are the same, we can take the second one to be anything different.
+			if(ab_MAX_indx === cd_MAX_indx) {
+				if(cd_MAX_indx === 0)
+					cd_MAX_indx = 1;
+				else
+					cd_MAX_indx = 0;
 			}
 		}
 		
-		//If both indices are the same, we can take the second one to be anything different.
-		if(ab_MAX_indx === cd_MAX_indx) {
-			if(cd_MAX_indx === 0)
-				cd_MAX_indx = 1;
-			else
-				cd_MAX_indx = 0;
-		}
+		//Projects a, b - a, c, d - c onto the a plane.
+		//Then, adapts the method from https://stackoverflow.com/a/565282/12419072
+		var p = [a.coordinates[ab_MAX_indx], a.coordinates[cd_MAX_indx]];
+		var r = [b.coordinates[ab_MAX_indx] - a.coordinates[ab_MAX_indx], b.coordinates[cd_MAX_indx] - a.coordinates[cd_MAX_indx]];
+		var q = [c.coordinates[ab_MAX_indx], c.coordinates[cd_MAX_indx]];
+		var s = [d.coordinates[ab_MAX_indx] - c.coordinates[ab_MAX_indx], d.coordinates[cd_MAX_indx] - c.coordinates[cd_MAX_indx]];
 		
-		//Projects a, b, c, d onto the a plane.
-		var a1 = [a.coordinates[ab_MAX_indx], a.coordinates[cd_MAX_indx]];
-		var b1 = [b.coordinates[ab_MAX_indx], b.coordinates[cd_MAX_indx]];
-		var c1 = [c.coordinates[ab_MAX_indx], c.coordinates[cd_MAX_indx]];
-		var d1 = [d.coordinates[ab_MAX_indx], d.coordinates[cd_MAX_indx]];
-		
-		//Adapts the method from https://stackoverflow.com/a/565282/12419072
-		var t = ((b1[0] - a1[0]) * d1[1] - (b1[1] - a1[1]) * d1[0])/(c1[0] * d1[1] - c1[1] * d1[0]);
-		return a.add(b.subtract(a).multiplyBy(t));
+		var t = ((p[0] - q[0]) * s[1] - (p[1] - q[1]) * s[0])/(r[1] * s[0] - r[0] * s[1]);
+		if(t <= 0 || t >= 1 || isNaN(t))
+			return null;
+		return Point.multiplyBy(Point.add(a,Point.subtract(b, a)), t);
 	}
 	
 	//Orders two points in lexicographic order of the coordinates.
