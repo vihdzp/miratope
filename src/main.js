4d4bf2d0c18115e7e258e6b47d9a9b3a806a6730
Zerocumulus <ultraroman01@gmail.com>
2020-10-26 17:34:24 -0500

    magic

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ const epsilon = 0.000000000001;
 //Configures the basic attributes of the scene.
 var mainScene = new Scene();
 
-//elementList is an array of arrays that contains all of a Polytope's elements:
+//elementList is an array of arrays of arrays that contains all of a Polytope's elements:
 //elementList = [[points], [edges], [faces],..., [ridges], [facets]]
 //A single array in elementList is itself a list of that type of element
 //The third edge of a polytope would be elementList[1][2]
