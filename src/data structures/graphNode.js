9881a82e948cc4fc43d120e76920f0dde3460496
OfficialURL <eric.ivan.hdz@gmail.com>
2020-10-25 22:46:26 -0600

    Minor improvements.

--- src/data structures/graphNode.js
+++ src/data structures/graphNode.js
@@ -5,25 +5,31 @@
 //Also not to be confused for the LinkedListNode class for doubly-linked lists
 
 function GraphNode(val) {
-	this.value = val; //Can be a number, a label, whatever
-	this.neighbors = []; //Contains neighboring graph nodes with optional labels (Format: [[neighborA, labelA], [neighborB, labelB], [neighborC, labelC], [neighborD, labelD]])
+	this.value = val; //The value of a node, can be any object.
+	this.neighbors = []; //Contains neighboring graph nodes.
+	this.labels = []; //Labels for edges, optional.
 	this.traversed = false; //Used for functions to tell if they have already processed a node
 };
 
 //Resets the traversed variable for an array of nodes
-//Should be helpful sometime else
+//Should be helpful sometime else.
 GraphNode.clearTraversed = function(nodes) {
 	for(i = 0, i < nodes.length; i++)
-		nodes[i].traversed = false
-}
+		nodes[i].traversed = false;
+};
 
 //Connects two nodes with one another, with an optional edge label (if I implemented this correctly)
-//Edge label defaults to "" if left undefined
+//Edge label added if specified.
 //Not meant to be called twice.
 //I could check whether the nodes are already connected, but that's costly.
-GraphNode.prototype.connectTo = function(node, name = "") {
-	this.neighbors.push([node, name]);
-	node.neighbors.push([this, name]);
+GraphNode.prototype.connectTo = function(node, name) {
+	this.neighbors.push(node);
+	node.neighbors.push(this);
+
+	if(name) {
+		this.labels.push(name);
+		node.labels.push(name);
+	}
 };
 
 //Gets the values of all nodes that are "attached" is some way
@@ -36,11 +42,11 @@ GraphNode.prototype.getComponent = function() {
 	}
 };
 
-//Private function does the hard work
+//Private function does the hard work of getComponent().
 GraphNode.prototype._getComponent = function() {
 	Node.components.push(this.value);
 	this.traversed = true;
 	for(var i = 0; i < this.neighbors.length; i++)
-		if(!this.neighbors[i][0].traversed)
-			this.neighbors[i][0]._getComponent();
-}
+		if(!this.neighbors[i].traversed)
+			this.neighbors[i]._getComponent();
+};
